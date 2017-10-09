6482e8b761c0dde543e8b163ee7f3a1d10b874c9
NateMietk <natemietkiewicz@gmail.com>
2017-10-09 16:32:43 -0600

    changed colors

--- src/R/map_data.R
+++ src/R/map_data.R
@@ -7,28 +7,28 @@ source("src/R/plot_theme.R")
 
 p <- ggplot() +
   
-  # map the raster
+  # # # map the raster
   geom_raster(data = forest, aes(x = x,
                                  y = y,
                                  fill = factor(forest_only),
                                  alpha = factor(forest_only)),
               show.legend = FALSE) +
   scale_alpha_discrete(name = "", range = c(0, 1), guide = F) +
-  scale_fill_manual(values = c("transparent", "forestgreen")) +
-  
+  scale_fill_manual(values = c("transparent", "green3")) +
+
   # map the neon domains
   geom_polygon(data=nd_df, aes(x = long, y = lat, group = group), 
                color='black', fill = "transparent", size = .10)+
   
   # map the forested sites that are not in the study
-  geom_point(data = nsf_df, aes(x = long, y = lat), size = 1, 
-             colour = "#000000", fill = NA, shape = 20) +
-  geom_point(data = nj_df, aes(x = long, y = lat), size = 1, 
-             colour = "#000000", fill = NA, shape = 20) +
+  geom_point(data = nsf_df, aes(x = long, y = lat, colour = group), size = 0.75, 
+             colour = "black", fill = 'yellow', shape = 21) +
+  geom_point(data = nj_df, aes(x = long, y = lat, colour = group), size = 0.75, 
+             colour = "black", fill = 'yellow', shape = 21) +
   
   # map the forested sites that are in the study
   geom_point(data = nks_df,  aes(x = long, y = lat), size = 1,
-             colour='#D62728', fill = NA, shape = 18) +
+             colour='#000000', fill = NA, shape = 18) +
   theme(legend.position = "none") +
   theme_map()
 
